0f28d186b7b4b60b9abecb3b768ebb140144a456
Ranel Tammel <raneltam@ad.helsinki.fi>
2022-11-14 14:06:38 +0200

    testing

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -1,10 +1,14 @@
 import unittest
+
 from maksukortti import Maksukortti
 
 
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
         self.kortti = Maksukortti(1000)
+
+    def test_alussa_oikein(self):
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10.00 euroa")
 
     def test_konstruktori_asettaa_saldon_oikein(self):
         self.assertEqual(str(self.kortti), "Kortilla on rahaa 10.00 euroa")
@@ -26,19 +30,24 @@ class TestMaksukortti(unittest.TestCase):
         self.assertEqual(str(kortti), "Kortilla on rahaa 2.00 euroa")
 
     def test_kortille_voi_ladata_rahaa(self):
-    	self.kortti.lataa_rahaa(2500)
+        self.kortti.lataa_rahaa(2500)
 
-    	self.assertEqual(str(self.kortti), "Kortilla on rahaa 35.00 euroa")
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 35.00 euroa")
     def test_kortin_saldo_ei_ylita_maksimiarvoa(self):
-    	self.kortti.lataa_rahaa(20000)
+        self.kortti.lataa_rahaa(20000)
 
-    	self.assertEqual(str(self.kortti), "Kortilla on rahaa 150.00 euroa")
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 150.00 euroa")
     def test_kortin_saldo_maukkaasti(self):
         self.kortti.syo_maukkaasti()
         self.kortti.syo_maukkaasti()
         self.kortti.syo_maukkaasti() #Meilla on vain  2 euro jaljella, ja siita emme saa pienentaa sita rahaa lainkaan
         self.assertEqual((str(self.kortti)),"Kortilla on rahaa 2.00 euroa")
     def test_ei_menee_negatiiviseksi(self):
-        kortti = Maksukortti(-999999)
+        kortti = Maksukortti(-3)
         self.assertEqual(str(kortti), "Kortilla on rahaa 2.00 euroa")    
-
+    def test_Rahan_lataaminen_kasvattaa_saldoa_oikein2(self):
+        self.kortti.lataa_rahaa(25100)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 250.00 euroa")
+        self.kortti.syo_maukkaasti()
+        self.kortti.syo_edullisesti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 240.50 euroa")
