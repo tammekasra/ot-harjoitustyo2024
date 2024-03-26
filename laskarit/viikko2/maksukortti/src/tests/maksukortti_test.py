9659d691b11f0f18b721a627915236c7cfda83b5
Ranel Tammela <ranel.tammela@helsinki.fi>
2024-03-26 15:32:50 +0200

    caches

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -1,6 +1,12 @@
 import unittest
 from maksukortti import Maksukortti
 
+
+    
+    
+    
+    
+
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
         self.kortti = Maksukortti(1000)
