b08adf7b64ae28bb6222e80cde4b63b1569dffd1
Ranel Tammela <ranel.tammela@helsinki.fi>
2024-03-26 15:43:45 +0200

    .coverage and caches

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -1,9 +1,22 @@
 import unittest
 from maksukortti import Maksukortti
 
+
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
-        self.maksukortti = Maksukortti(1000)
+        self.kortti = Maksukortti(1000)  
 
-    def test_luotu_kortti_on_olemassa(self):
-        self.assertNotEqual(self.maksukortti, None)
+    def test_saldo_oikein(self):
+        self.assertEqual(self.kortti.saldo, 1000)
+
+    def test_rahan_lataaminen_oikein(self):
+        self.kortti.lataa_rahaa(500)
+        self.assertEqual(self.kortti.saldo, 1500)
+
+    def test_rahan_riittavasti(self):
+        self.assertTrue(self.kortti.ota_rahaa(500))
+        self.assertEqual(self.kortti.saldo, 500)
+
+    def test_rahan_ei_riittavasti(self):
+        self.assertFalse(self.kortti.ota_rahaa(1500)) 
+        self.assertEqual(self.kortti.saldo, 1000) 
