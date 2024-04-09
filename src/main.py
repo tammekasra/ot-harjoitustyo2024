8337a7427f79235ac74a3ede973378bd44f2b88f
Ranel Tammela <ranel.tammela@helsinki.fi>
2024-04-09 22:20:34 +0300

    main.py with tests

---
+++ src/main.py
@@ -0,0 +1,22 @@
+def print_sudoku_board(board):
+    """
+    this just print it as a  board for display
+    """
+    for row in board:
+        print(" ".join(map(str, row)))
+
+# Example Sudoku board
+sudoku_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+# Prints it
+print_sudoku_board(sudoku_board)
