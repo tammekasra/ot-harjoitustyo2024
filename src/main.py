5d83fbbc8e93e8fe89c0cd9833ea00365da3463f
Ranel Tammela <ranel.tammela@helsinki.fi>
2024-05-07 17:42:53 +0300

    This should work now!

--- src/main.py
+++ src/main.py
@@ -2,13 +2,21 @@
 from Sudoku import user_interface
 
 
+
+
 if __name__ == "__main__":
-
+    
     a = user_interface.UserInterface2()
-
+    
     game_loop = True
     while game_loop:
-
+    
         a.input_box()
-
+        
         a.run()
+
+    
+    
+
+
+
